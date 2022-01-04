b327c839be19078e7e82d434de8317b4a5d7c4bb
Aleksey Timin <atimin@gmail.com>
2022-01-04 20:32:53 +0100

    integrate entries to the bucket

--- src/reduct/storage/bucket.cc
+++ src/reduct/storage/bucket.cc
@@ -1,25 +1,81 @@
 // Copyright 2021-2022 Alexey Timin
-
 #include "reduct/storage/bucket.h"
 
+#include <ranges>
+
+#include "reduct/config.h"
 #include "reduct/core/logger.h"
 
 namespace reduct::storage {
 
 namespace fs = std::filesystem;
+using core::Error;
 
 class Bucket : public IBucket {
  public:
-  explicit Bucket(Options options) : options_(std::move(options)) {
+  explicit Bucket(Options options) : options_(std::move(options)), entry_map_() {
     auto full_path = options_.path / options_.name;
     if (!fs::exists(full_path)) {
       LOG_DEBUG("Path {} doesn't exist. Create a new bucket.", full_path.string());
       fs::create_directories(full_path);
+    } else if (fs::is_directory(full_path)) {
+      for (const auto& folder : fs::directory_iterator(full_path)) {
+        if (fs::is_directory(folder)) {
+          auto entry_name = folder.path().filename().string();
+          auto entry = IEntry::Restore(folder);
+          if (entry) {
+            entry_map_[entry_name] = std::move(entry);
+          } else {
+            LOG_ERROR("Failed to restore entry '{}'", entry_name);
+          }
+        }
+      }
     }
+  }
+
+  EntryRef GetOrCreateEntry(const std::string& name) override {
+    auto it = entry_map_.find(name);
+    if (it != entry_map_.end()) {
+      return {it->second, Error::kOk};
+    } else {
+      LOG_DEBUG("No '{}' entry in a bucket. Try to create one");
+      auto entry = IEntry::Build({
+          .name = name,
+          .path = options_.path / options_.name,
+          .max_block_size = kDefaultMaxBlockSize,
+      });
+
+      if (entry) {
+        std::shared_ptr<IEntry> ptr = std::move(entry);
+        entry_map_[name] = ptr;
+        return {ptr, Error::kOk};
+      }
+    }
+
+    return {std::weak_ptr<IEntry>(), Error{.code = 500, .message = fmt::format("Failed to create bucket '{}'", name)}};
+  }
+
+  Error Clean() override {
+    for (auto& [name, entry] : entry_map_) {
+      const auto entry_path = options_.path / options_.name / name;
+      if (fs::exists(entry_path)) {
+        fs::remove_all(entry_path);
+      }
+    }
+
+    entry_map_ = {};
+    return Error::kOk;
+  }
+
+  [[nodiscard]] Info GetInfo() const override {
+    return {
+        .entry_count = entry_map_.size(),
+    };
   }
 
  private:
   Options options_;
+  std::map<std::string, std::shared_ptr<IEntry>> entry_map_;
 };
 
 std::unique_ptr<IBucket> IBucket::Build(const Options& options) {
@@ -31,7 +87,7 @@ std::unique_ptr<IBucket> IBucket::Build(const Options& options) {
   std::unique_ptr<IBucket> bucket;
   try {
     bucket = std::make_unique<Bucket>(options);
-  } catch (const fs::filesystem_error &err) {
+  } catch (const fs::filesystem_error& err) {
     LOG_ERROR("Failed create bucket '{}': {}", options.name, err.what());
   }
 
--- unit_tests/reduct/storage/bucket_test.cc
+++ unit_tests/reduct/storage/bucket_test.cc
@@ -6,7 +6,9 @@
 
 #include "reduct/helpers.h"
 
+using reduct::core::Error;
 using reduct::storage::IBucket;
+using reduct::storage::IEntry;
 
 namespace fs = std::filesystem;
 
@@ -17,15 +19,46 @@ TEST_CASE("storage::Bucket should create folder", "[bucket]") {
   REQUIRE(bucket);
   REQUIRE(fs::exists(dir_path));
 
-  SECTION("it is ok, if directory already exist") {
-    REQUIRE(IBucket::Build({.name = "bucket", .path = dir_path}));
-  }
+  SECTION("it is ok, if directory already exist") { REQUIRE(IBucket::Build({.name = "bucket", .path = dir_path})); }
 
   SECTION("return nullptr if something got wrong") {
     REQUIRE_FALSE(IBucket::Build({.name = "", .path = "/non-existing/path"}));
   }
 
-  SECTION("name cannot be empty") {
-    REQUIRE_FALSE(IBucket::Build({.name = "", .path = dir_path}));
+  SECTION("name cannot be empty") { REQUIRE_FALSE(IBucket::Build({.name = "", .path = dir_path})); }
+}
+
+TEST_CASE("storage::Bucket should create get or create folder", "[bucket][entry]") {
+  auto bucket = IBucket::Build({.name = "bucket", .path = BuildTmpDirectory()});
+
+  SECTION("create a new bucket") {
+    auto [entry, err] = bucket->GetOrCreateEntry("entry_1");
+    REQUIRE(err == Error::kOk);
+    REQUIRE(entry.lock());
   }
+
+  SECTION("get an existing bucket") {
+    auto ref = bucket->GetOrCreateEntry("entry_1");
+    REQUIRE(ref.error == Error::kOk);
+    REQUIRE(ref.entry.lock()->GetInfo().record_count == 0);
+    REQUIRE(ref.entry.lock()->Write("some_blob", IEntry::Time::clock::now()) == Error::kOk);
+
+    ref = bucket->GetOrCreateEntry("entry_1");
+    REQUIRE(ref.error == Error::kOk);
+    REQUIRE(ref.entry.lock()->GetInfo().record_count == 1);
+  }
+}
+
+TEST_CASE("storage::Bucket should remove all bucket", "[bucket]") {
+  auto dir_path = BuildTmpDirectory();
+  auto bucket = IBucket::Build({.name = "bucket", .path = dir_path});
+
+  REQUIRE(bucket->GetOrCreateEntry("entry_1").error == Error::kOk);
+  REQUIRE(bucket->GetOrCreateEntry("entry_2").error == Error::kOk);
+  REQUIRE(bucket->GetInfo().entry_count == 2);
+
+  REQUIRE(bucket->Clean() == Error::kOk);
+  REQUIRE(bucket->GetInfo().entry_count == 0);
+  REQUIRE_FALSE(fs::exists(dir_path / "bucket" / "entry_1"));
+  REQUIRE_FALSE(fs::exists(dir_path / "bucket" / "entry_2"));
 }
