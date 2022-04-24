da0f688377e8ee426a5ffc2bdab7572c1c419655
Aleksey Timin <atimin@gmail.com>
2022-04-24 14:41:07 +0200

    add unit tests

---
+++ unit_tests/reduct/asset/asset_manager_test.cc
@@ -0,0 +1,35 @@
+// Copyright 2022 Reduct Storage Team
+
+#include "reduct/asset/asset_manager.h"
+
+#include <catch2/catch.hpp>
+#include <libzippp/libzippp.h>
+
+#include <fstream>
+
+using reduct::asset::IAssetManager;
+using reduct::core::Error;
+
+TEST_CASE("asset::IAssetManager should have empty implementation") {
+  auto empty = IAssetManager::BuildEmpty();
+  REQUIRE(empty->Read("/").error == Error{.code = 404, .message = "No static files supported"});
+}
+
+TEST_CASE("asset::IAssetManager should extract files form zip") {
+  using libzippp::ZipArchive;
+
+  const auto tmp = std::filesystem::temp_directory_path();
+  ZipArchive zf(tmp / "archive.zip");
+  zf.open(ZipArchive::Write);
+  zf.addData("helloworld.txt", "Hello,World!", 12);
+  zf.close();
+
+  std::ifstream arch(tmp / "archive.zip");
+  std::stringstream ss;
+  ss << arch.rdbuf();
+
+  auto zip_asset = IAssetManager::BuildFromZip(ss.str());
+
+  REQUIRE(zip_asset);
+  REQUIRE(zip_asset->Read("helloworld.txt").result == "Hello,World!");
+}
