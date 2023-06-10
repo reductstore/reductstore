d49d9cc8a8a5031c767fbee6d26abfb53035c581
renghen <renghen@yahoo.com>
2023-06-10 22:12:36 +0400

    test python updated

--- api_tests/http_test.py
+++ api_tests/http_test.py
@@ -8,3 +8,17 @@
 #
 #     assert resp.status_code == 200
 #     assert resp.headers['access-control-allow-origin'] == session.headers['origin']
+
+def test_allow_origin(base_url, session):
+    """Should get api version in header"""
+    # session.headers['origin'] = 'http://test.com'
+    resp = session.get(f'{base_url}/info')
+
+    version = resp.headers['ReductStore']
+    tokens = version.split(".", 3)
+    expected_version = tokens[0] + "." + tokens[1]
+
+    assert resp.status_code == 200
+    assert resp.headers['x-reduct-api'] == expected_version
+
+
