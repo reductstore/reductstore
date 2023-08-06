74da47768734e9e8f01242ecb954194a34bdaaf5
Alexey Timin <atimin@gmail.com>
2023-08-06 22:52:28 +0200

    add example

---
+++ reduct_rs/examples/hallo_world.rs
@@ -0,0 +1,57 @@
+// Copyright 2023 ReductStore
+// This Source Code Form is subject to the terms of the Mozilla Public
+//    License, v. 2.0. If a copy of the MPL was not distributed with this
+//    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use bytes::Bytes;
+use futures_util::StreamExt;
+use reduct_base::error::HttpError;
+use reduct_rs::ReductClient;
+use std::str::from_utf8;
+use std::time::SystemTime;
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), HttpError> {
+    let client = ReductClient::builder()
+        .url("http://127.0.0.1:8383")
+        .api_token("TOKEN")
+        .build();
+
+    println!("Server v{:?}", client.server_info().await?.version);
+
+    let bucket = client.create_bucket("test").exist_ok(true).send().await?;
+
+    bucket
+        .write_record("entry-1")
+        .add_label("planet", "Earth")
+        .data(Bytes::from("Hello, Earth!"))
+        .send()
+        .await?;
+
+    bucket
+        .write_record("entry-1")
+        .add_label("planet", "Mars")
+        .data(Bytes::from("Hello, Mars!"))
+        .send()
+        .await?;
+
+    let mut query = bucket
+        .query("entry-1")
+        .add_include("planet", "Earth")
+        .send()
+        .await?;
+
+    tokio::pin!(query);
+
+    while let Some(record) = query.next().await {
+        let record = record?;
+        println!("Record: {:?}", record);
+        println!(
+            "Data: {}",
+            from_utf8(&record.bytes().await?.to_vec()).unwrap()
+        );
+    }
+
+    Ok(())
+}
