6a612acdd6d658a7f015d7001ffbcdf59d773e99
Alexey Timin <atimin@gmail.com>
2023-11-09 10:38:25 +0100

    fix reduct-cli tests

--- reduct_cli/src/cmd/bucket/show.rs
+++ reduct_cli/src/cmd/bucket/show.rs
@@ -203,13 +203,13 @@ mod tests {
             context.stdout().history(),
             vec!["Name:                test_bucket                    Quota Type:         NONE",
                  "Entries:             1                              Quota Size:         0 B",
-                 "Size:                8 B                            Max. Block Size:    64.0 MB",
+                 "Size:                77 B                           Max. Block Size:    64.0 MB",
                  "Oldest Record (UTC): 1970-01-01T00:00:00.000Z       Max. Block Records: 256",
                  "Latest Record (UTC): 1970-01-01T00:00:00.001Z      ",
                  "----------------------------- | ----------| ----------| ---------- | ------------------------------ | ------------------------------|",
                  "Name                          | Blocks    | Records   | Size       | Oldest record (UTC)            | Latest record (UTC)           |",
                  "----------------------------- | ----------| ----------| ---------- | ------------------------------ | ------------------------------|",
-                 "test                          |          1|          2| 8 B        | 1970-01-01T00:00:00.000Z       | 1970-01-01T00:00:00.001Z      |"]
+                 "test                          |          1|          2| 77 B       | 1970-01-01T00:00:00.000Z       | 1970-01-01T00:00:00.001Z      |"]
         );
     }
 }
