1090609f30dd988157e79bc5841cd4ec0b455d15
Alexey Timin <atimin@gmail.com>
2024-02-25 00:04:21 +0100

    speed up start of database

--- reductstore/src/storage/bucket.rs
+++ reductstore/src/storage/bucket.rs
@@ -589,13 +589,13 @@ mod tests {
         let blob: &[u8] = &[0u8; 40];
 
         write(&mut bucket, "test-1", 0, blob).await.unwrap();
-        assert_eq!(bucket.info().await.unwrap().info.size, 44);
+        assert_eq!(bucket.info().await.unwrap().info.size, 52);
 
         write(&mut bucket, "test-2", 1, blob).await.unwrap();
-        assert_eq!(bucket.info().await.unwrap().info.size, 91);
+        assert_eq!(bucket.info().await.unwrap().info.size, 110);
 
         write(&mut bucket, "test-3", 2, blob).await.unwrap();
-        assert_eq!(bucket.info().await.unwrap().info.size, 94);
+        assert_eq!(bucket.info().await.unwrap().info.size, 116);
 
         assert_eq!(
             read(&mut bucket, "test-1", 0).await.err(),
@@ -619,7 +619,7 @@ mod tests {
         );
 
         write(&mut bucket, "test-1", 0, b"test").await.unwrap();
-        assert_eq!(bucket.info().await.unwrap().info.size, 8);
+        assert_eq!(bucket.info().await.unwrap().info.size, 16);
 
         let result = write(&mut bucket, "test-2", 1, b"0123456789___").await;
         assert_eq!(
