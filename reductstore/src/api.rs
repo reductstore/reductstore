b9f16e37b298a2d57bf8bc202de07b7014e23cde
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-07-30 00:43:30 +0200

    add CORS configuration

--- reductstore/src/api.rs
+++ reductstore/src/api.rs
@@ -9,8 +9,10 @@ use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::{middleware::from_fn, Router};
+use hyper::http::HeaderValue;
 use serde::de::StdError;
 use tokio::sync::RwLock;
+use tower_http::cors::{Any, CorsLayer};
 
 pub use reduct_base::error::ErrorCode;
 use reduct_base::error::ReductError as BaseHttpError;
@@ -103,8 +105,13 @@ impl From<serde_json::Error> for HttpError {
     }
 }
 
-pub fn create_axum_app(api_base_path: &String, components: Arc<Components>) -> Router {
+pub fn create_axum_app(
+    api_base_path: &String,
+    cors_allow_origin: &Vec<String>,
+    components: Arc<Components>,
+) -> Router {
     let b_route = create_bucket_api_routes().merge(create_entry_api_routes());
+    let cors = configure_cors(cors_allow_origin);
 
     let app = Router::new()
         // Server API
@@ -129,8 +136,28 @@ pub fn create_axum_app(api_base_path: &String, components: Arc<Components>) -> R
         .fallback(get(show_ui))
         .layer(from_fn(default_headers))
         .layer(from_fn(print_statuses))
+        .layer(cors)
         .with_state(components);
     app
+}
+
+fn configure_cors(cors_allow_origin: &Vec<String>) -> CorsLayer {
+    if cors_allow_origin.contains(&"*".to_string()) {
+        CorsLayer::new()
+            .allow_origin(Any)
+            .allow_methods(Any)
+            .allow_headers(Any)
+    } else {
+        let parsed_origins: Vec<HeaderValue> = cors_allow_origin
+            .iter()
+            .map(|origin| origin.parse::<HeaderValue>().unwrap())
+            .collect();
+
+        CorsLayer::new()
+            .allow_origin(parsed_origins)
+            .allow_methods(Any)
+            .allow_headers(Any)
+    }
 }
 
 #[cfg(test)]
