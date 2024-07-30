f9c2b36d014ded282f0cdc603458f75330c12544
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-07-30 22:02:02 +0200

    use session fixture in cors tests

--- integration_tests/api/http_test.py
+++ integration_tests/api/http_test.py
@@ -1,7 +1,5 @@
 """Test basic HTTP things"""
 
-import requests
-
 
 def test_api_version(base_url, session):
     """Should get api version in header"""
@@ -11,10 +9,10 @@ def test_api_version(base_url, session):
     assert resp.headers["x-reduct-api"] == "1.11"
 
 
-def test_cors_allows_first_allowed_origin(base_url):
+def test_cors_allows_first_allowed_origin(base_url, session):
     """Should allow requests from the first allowed origin"""
     headers = {"Origin": "https://first-allowed-origin.com"}
-    resp = requests.get(f"{base_url}/info", headers=headers)
+    resp = session.get(f"{base_url}/info", headers=headers)
     assert resp.status_code == 200
     assert (
         resp.headers["Access-Control-Allow-Origin"]
@@ -22,10 +20,10 @@ def test_cors_allows_first_allowed_origin(base_url):
     )
 
 
-def test_cors_allows_second_allowed_origin(base_url):
+def test_cors_allows_second_allowed_origin(base_url, session):
     """Should allow requests from the second allowed origin"""
     headers = {"Origin": "https://second-allowed-origin.com"}
-    resp = requests.get(f"{base_url}/info", headers=headers)
+    resp = session.get(f"{base_url}/info", headers=headers)
     assert resp.status_code == 200
     assert (
         resp.headers["Access-Control-Allow-Origin"]
@@ -33,25 +31,25 @@ def test_cors_allows_second_allowed_origin(base_url):
     )
 
 
-def test_cors_disallowed_origin(base_url):
+def test_cors_disallowed_origin(base_url, session):
     """Should not allow requests from a disallowed origin"""
     headers = {"Origin": "https://disallowed-origin.com"}
-    resp = requests.get(f"{base_url}/info", headers=headers)
+    resp = session.get(f"{base_url}/info", headers=headers)
     assert resp.status_code == 200
     assert "Access-Control-Allow-Origin" not in resp.headers
 
 
-def test_cors_allowed_methods(base_url):
+def test_cors_allowed_methods(base_url, session):
     """Should allow requests with any method"""
     headers = {"Origin": "https://first-allowed-origin.com"}
-    resp = requests.options(f"{base_url}/info", headers=headers)
+    resp = session.options(f"{base_url}/info", headers=headers)
     assert resp.status_code == 200
     assert resp.headers["Access-Control-Allow-Methods"] == "*"
 
 
-def test_cors_allowed_headers(base_url):
+def test_cors_allowed_headers(base_url, session):
     """Should allow requests with any headers"""
     headers = {"Origin": "https://first-allowed-origin.com"}
-    resp = requests.options(f"{base_url}/info", headers=headers)
+    resp = session.options(f"{base_url}/info", headers=headers)
     assert resp.status_code == 200
     assert resp.headers["Access-Control-Allow-Headers"] == "*"
