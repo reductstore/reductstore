891549630654a23e95ad7967c4fcafb270206dd2
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-07-30 10:44:25 +0200

    cors integration tests

--- integration_tests/api/http_test.py
+++ integration_tests/api/http_test.py
@@ -1,5 +1,7 @@
 """Test basic HTTP things"""
 
+import requests
+
 
 def test_api_version(base_url, session):
     """Should get api version in header"""
@@ -7,3 +9,49 @@ def test_api_version(base_url, session):
 
     assert resp.status_code == 200
     assert resp.headers["x-reduct-api"] == "1.11"
+
+
+def test_cors_allows_first_allowed_origin(base_url):
+    """Should allow requests from the first allowed origin"""
+    headers = {"Origin": "https://first-allowed-origin.com"}
+    resp = requests.get(f"{base_url}/info", headers=headers)
+    assert resp.status_code == 200
+    assert (
+        resp.headers["Access-Control-Allow-Origin"]
+        == "https://first-allowed-origin.com"
+    )
+
+
+def test_cors_allows_second_allowed_origin(base_url):
+    """Should allow requests from the second allowed origin"""
+    headers = {"Origin": "https://second-allowed-origin.com"}
+    resp = requests.get(f"{base_url}/info", headers=headers)
+    assert resp.status_code == 200
+    assert (
+        resp.headers["Access-Control-Allow-Origin"]
+        == "https://second-allowed-origin.com"
+    )
+
+
+def test_cors_disallowed_origin(base_url):
+    """Should not allow requests from a disallowed origin"""
+    headers = {"Origin": "https://disallowed-origin.com"}
+    resp = requests.get(f"{base_url}/info", headers=headers)
+    assert resp.status_code == 200
+    assert "Access-Control-Allow-Origin" not in resp.headers
+
+
+def test_cors_allowed_methods(base_url):
+    """Should allow requests with any method"""
+    headers = {"Origin": "https://first-allowed-origin.com"}
+    resp = requests.options(f"{base_url}/info", headers=headers)
+    assert resp.status_code == 200
+    assert resp.headers["Access-Control-Allow-Methods"] == "*"
+
+
+def test_cors_allowed_headers(base_url):
+    """Should allow requests with any headers"""
+    headers = {"Origin": "https://first-allowed-origin.com"}
+    resp = requests.options(f"{base_url}/info", headers=headers)
+    assert resp.status_code == 200
+    assert resp.headers["Access-Control-Allow-Headers"] == "*"
