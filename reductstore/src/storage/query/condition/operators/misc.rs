c5a33cc569bbd9d3773af27ef5fd8892db351c00
Alexey Timin <atimin@gmail.com>
2025-02-04 11:46:34 +0100

    implement $exists operator

---
+++ reductstore/src/storage/query/condition/operators/misc.rs
@@ -0,0 +1,6 @@
+// Copyright 2025 ReductSoftware UG
+// Licensed under the Business Source License 1.1
+
+mod exists;
+
+pub(crate) use exists::Exists;
--- reductstore/src/storage/query/condition/parser.rs
+++ reductstore/src/storage/query/condition/parser.rs
@@ -7,6 +7,7 @@ use crate::storage::query::condition::operators::arithmetic::{
 };
 use crate::storage::query::condition::operators::comparison::{Eq, Gt, Gte, Lt, Lte, Ne};
 use crate::storage::query::condition::operators::logical::{AllOf, AnyOf, NoneOf, OneOf};
+use crate::storage::query::condition::operators::misc::Exists;
 use crate::storage::query::condition::operators::string::{Contains, EndsWith, StartsWith};
 use crate::storage::query::condition::reference::Reference;
 use crate::storage::query::condition::value::Value;
@@ -130,6 +131,9 @@ impl Parser {
             "$contains" => Contains::boxed(operands),
             "$starts_with" => StartsWith::boxed(operands),
             "$ends_with" => EndsWith::boxed(operands),
+
+            // Misc
+            "$exists" => Exists::boxed(operands),
 
             _ => Err(unprocessable_entity!(
                 "Operator '{}' not supported",
@@ -265,6 +269,8 @@ mod tests {
         #[case("$contains", "[\"abc\", \"b\"]", Value::Bool(true))]
         #[case("$starts_with", "[\"abc\", \"ab\"]", Value::Bool(true))]
         #[case("$ends_with", "[\"abc\", \"bc\"]", Value::Bool(true))]
+        // Misc
+        #[case("$exists", "[\"label\"]", Value::Bool(false))]
         fn test_parse_operator(
             parser: Parser,
             context: Context,
