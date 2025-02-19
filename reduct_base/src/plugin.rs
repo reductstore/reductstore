9c9fec2c491f4313c563100902517728e4e294f2
Alexey Timin <atimin@gmail.com>
2025-02-19 10:38:31 +0100

    add builder pattern

--- reduct_base/src/plugin.rs
+++ reduct_base/src/plugin.rs
@@ -3,6 +3,10 @@
 //    License, v. 2.0. If a copy of the MPL was not distributed with this
 //    file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use derive_builder::Builder;
+
+#[derive(Builder)]
+#[builder(setter(into))]
 pub struct PluginInfo {
     name: String,
     version: String,
