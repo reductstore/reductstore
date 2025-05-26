a8764645714d436909b6933cb402bc656c547fef
Alexey Timin <atimin@gmail.com>
2025-05-26 17:49:37 +0200

    fix extension version

--- reductstore/src/ext/ext_repository/load.rs
+++ reductstore/src/ext/ext_repository/load.rs
@@ -13,6 +13,8 @@ use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::sync::RwLock as AsyncRwLock;
 
+const EXTENSION_VERSION: &str = "0.2.3";
+
 impl ExtRepository {
     pub(super) fn try_load(
         paths: Vec<PathBuf>,
@@ -94,7 +96,7 @@ mod tests {
             info,
             IoExtensionInfo::builder()
                 .name("test-ext")
-                .version("0.2.2")
+                .version(EXTENSION_VERSION)
                 .build()
         );
     }
@@ -128,7 +130,6 @@ mod tests {
     #[fixture]
     fn ext_repo(ext_settings: ExtSettings) -> ExtRepository {
         // This is the path to the build directory of the extension from ext_stub crate
-        const EXTENSION_VERSION: &str = "0.2.3";
 
         let file_name = if cfg!(target_os = "linux") {
             // This is the path to the build directory of the extension from ext_stub crate
