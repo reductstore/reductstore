f29431be8a7e4d47aa18680cffdd34301861c770
Alexey Timin <atimin@gmail.com>
2025-09-23 16:54:40 +0200

    make records public

--- reduct_base/src/io.rs
+++ reduct_base/src/io.rs
@@ -3,7 +3,7 @@
 //    License, v. 2.0. If a copy of the MPL was not distributed with this
 //    file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-mod records;
+pub mod records;
 
 use crate::error::ReductError;
 use crate::Labels;
