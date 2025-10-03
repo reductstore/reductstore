001dc9f38dd1f6eb629c6fe19e2a20b11adfb336
Alexey Timin <atimin@gmail.com>
2025-10-03 23:03:38 +0200

    update CHANGELOG

--- reductstore/src/storage/storage.rs
+++ reductstore/src/storage/storage.rs
@@ -312,6 +312,7 @@ pub(super) fn check_name_convention(name: &str) -> Result<(), ReductError> {
 mod tests {
     use super::*;
     use crate::backend::Backend;
+    use crate::cfg::remote_storage::RemoteStorageConfig;
     use bytes::Bytes;
     use reduct_base::msg::bucket_api::QuotaType;
     use reduct_base::Labels;
