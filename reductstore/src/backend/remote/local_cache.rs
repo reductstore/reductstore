140da1e053d3438fb1acf49c6a8b29bb53d87790
Alexey Timin <atimin@gmail.com>
2025-10-13 14:00:14 +0200

    local cache must be cleaned not removed

--- reductstore/src/backend/remote/local_cache.rs
+++ reductstore/src/backend/remote/local_cache.rs
@@ -26,6 +26,8 @@ impl LocalCache {
             }
         }
 
+        fs::create_dir_all(&path).expect("Failed to create local cache directory");
+
         LocalCache {
             path,
             max_size,
