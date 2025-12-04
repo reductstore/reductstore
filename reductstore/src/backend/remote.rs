2060712f9910d52f087b5be92a8500194d173e69
Alexey Timin <atimin@gmail.com>
2025-12-04 13:55:22 +0100

    add test

--- reductstore/src/backend/remote.rs
+++ reductstore/src/backend/remote.rs
@@ -582,6 +582,30 @@ mod tests {
 
             remote_backend.download(&file_path).unwrap();
         }
+
+        #[rstest]
+        fn test_download_file_with_creating_directory(
+            mut mock_connector: MockRemoteStorageConnector,
+            path: PathBuf,
+        ) {
+            let file_key = "subdir/file1.txt";
+            let file_path = path.join(file_key);
+
+            mock_connector
+                .expect_download_object()
+                .with(eq(file_key), eq(path.join(file_key).to_path_buf()))
+                .times(1)
+                .returning(|_, path| {
+                    fs::create_dir_all(path.parent().unwrap()).unwrap();
+                    fs::write(path.clone(), b"test").unwrap(); // we need to create the file to register it in cache
+                    Ok(())
+                });
+            let remote_backend = make_remote_backend(mock_connector, path.clone());
+
+            remote_backend.download(&file_path).unwrap();
+
+            assert!(file_path.exists());
+        }
     }
 
     mod update_local_cache {
