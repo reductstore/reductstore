91854b4845d8db6a6294b074dd05c8f622763030
Alexey Timin <atimin@gmail.com>
2026-06-29 17:40:05 +0200

    invalidate block in cache if it's not updated

--- reductstore/src/storage/block_manager.rs
+++ reductstore/src/storage/block_manager.rs
@@ -220,6 +220,7 @@ impl BlockManager {
                                 block_crc,
                                 crc.sum64()
                             );
+                            self.invalidate_replica_block_cache(block_id).await?;
                             self.block_index.remove_block(block_id);
                             return Err(too_early!(
                                 "Block descriptor {:?} CRC mismatch on replica. Reload index and retry",
@@ -766,6 +767,16 @@ impl BlockManager {
 
     pub(in crate::storage) fn is_replica(&self) -> bool {
         self.cfg.role == InstanceRole::Replica
+    }
+
+    async fn invalidate_replica_block_cache(&self, block_id: u64) -> Result<(), ReductError> {
+        FILE_CACHE
+            .invalidate_local_cache_file(&self.path_to_desc(block_id))
+            .await?;
+        FILE_CACHE
+            .invalidate_local_cache_file(&self.path_to_data(block_id))
+            .await?;
+        Ok(())
     }
 
     pub(in crate::storage) fn is_block_in_write_cache(&self, block_id: u64) -> bool {
